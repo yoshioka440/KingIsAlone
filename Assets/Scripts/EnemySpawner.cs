3c37de980afc1da46fe983d9360ecf070df52aeb
clear10 <pg.clear10@gmail.com>
2015-04-18 21:49:32 +0900

    no message

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class EnemySpawner : MonoBehaviour {
 
@@ -11,5 +12,13 @@ public class EnemySpawner : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	List<Enemy> ImportMtbEnemys () {
+		Entity_SpawnEnemy entityEnemy = Resources.Load<Entity_SpawnEnemy>("Data/mtb_enemy_spawn_rate");
+		List<Enemy> enemies = new List<Enemy>();
+
+
+		return enemies;
 	}
 }
