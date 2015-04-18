d40f6775e98ed9b99fe2ebfc8794301c295c5978
nyarurato <yano@nyaru.local>
2015-04-18 18:14:05 +0900

    Add Bullet.cs & Magazine.cs

---
+++ Assets/Magaizine.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+
+public class Magaizine {
+
+	int initial_bullet;
+	BulletType[][] bullet_arrays;
+	int[] now_bullet_num;
+
+	public Magaizine(){
+
+		initial_bullet = 200;
+		bullet_arrays = new BulletType[3][];
+
+		now_bullet_num = new int[3] {0,0,0};
+		Start();
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		//大砲ごとのbullet
+		for(int i=0;i<3;i++){
+			bullet_arrays[i] = new BulletType[initial_bullet];
+
+			bullet_arrays[i] = MakeBulletsList(initial_bullet);
+		}
+	}
+
+	BulletType[] MakeBulletsList(int bullet_num){
+		BulletType[] bullet = new BulletType[bullet_num];
+		System.Random random = new System.Random ();
+
+		for (int i = 0; i < bullet.Length; i++) {
+
+			bullet [i] = (BulletType)random.Next (Enum.GetNames (typeof(BulletType)).Length);
+		}
+		return bullet;
+	}
+
+
+	public BulletType NowBullet(int shooter_num){
+		return bullet_arrays [shooter_num][now_bullet_num [shooter_num]];
+	}
+
+	public BulletType NextBullet(int shooter_num){
+		return bullet_arrays [shooter_num][now_bullet_num[shooter_num]+1];
+	}
+
+	public BulletType NextNextBullet(int shooter_num){
+		return bullet_arrays [shooter_num][now_bullet_num[shooter_num]+2];
+	}
+
+	public void Next(int shooter_num){
+
+		if (now_bullet_num[shooter_num] < initial_bullet-2) {
+			now_bullet_num [shooter_num]++;	
+		} else {
+			now_bullet_num [shooter_num] = 0;
+		}
+	}
+
+}
+
+public enum BulletType{
+	Desk,
+	Bed,
+	Can,
+	Spoon,
+	Fork,
+	Planter
+};
