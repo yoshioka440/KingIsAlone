bd84943769b3c7d78585cb9d59ae4abc249da472
yoshioka440 <yoshioka440@gmail.com>
2015-04-18 14:34:04 +0900

    Hello Git.
    Hello Git.

---
+++ Assets/GitTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GitTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Hello Git!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
