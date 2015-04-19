6e31badfbe75349d9d34a7799ff56126e36b5f6d
nyarurato <yano@nyaru.local>
2015-04-19 14:04:31 +0900

    Update Enemy.cd Shooter.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,18 +4,24 @@ using System.Collections;
 public class Enemy : MonoBehaviour {
 
 	int hitPoint = -1;
-	float speed = 1f;
+	[SerializeField]float speed = 5f;
 	Vector3 travelling = new Vector3();
+	bool move_flag = true;
+	Rigidbody rbody;
+	[SerializeField]float wait_time;
 
 	// Use this for initialization
 	void Start () {
 		//hitPoint = 100;
 		travelling = Vector3.left;
+		rbody = gameObject.GetComponent<Rigidbody> ();
+		StartCoroutine ("Move");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(travelling * Time.deltaTime * speed, Space.World);
+
+		//transform.Translate(travelling * Time.deltaTime * speed, Space.World);
 	}
 
 	void OnCollisionEnter (Collision col) {
@@ -27,5 +33,13 @@ public class Enemy : MonoBehaviour {
 	public void Init (int hp, float speed) {
 		this.hitPoint = hp;
 		this.speed = speed;
+	}
+
+	IEnumerator Move(){
+
+		while (move_flag) {
+			rbody.velocity = travelling * speed;
+			yield return new WaitForSeconds (wait_time); 
+		}
 	}
 }
