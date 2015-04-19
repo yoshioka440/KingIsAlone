334228d185b13b9abeb426f2108fa02d5a88fe47
yoshioka440 <yoshioka440@gmail.com>
2015-04-19 19:49:01 +0900

    Add NextBoxUI.cs

--- Assets/Scripts/NextBoxUI.cs
+++ Assets/Scripts/NextBoxUI.cs
@@ -4,56 +4,71 @@ using UnityEngine.UI;
 
 public class NextBoxUI : MonoBehaviour {
 
-	// プレハブオブジェクト
-	public GameObject currentBulletUI;
-	public GameObject nextBulletUI;
-	public GameObject nextnextBulletUI;
-
 	// UIオブジェクト
-	public GameObject currentBullet;
+	public GameObject currentBulletImage;
+	public GameObject nextBulletImage;
+	public GameObject nextnextBulletImage;
+
+	// 格納用オブジェクト
+	//public GameObject currentBullet;
 	public GameObject nextBullet;
 	public GameObject nextnextBullet;
+
+	// プレハブオブジェクト(テスト用)
+	public GameObject bulletPrefab;
 
 	// Magagine
 	public GameObject magazine;
 
-	// 
-	private int playerSelectedShooterNum;
+	// プレイヤーからの受け取り用変数
+	private int selectedShooterNum;
+	// Magazineからの取得用
+	private BulletType[] bullet;
+
+	// Sprites取得
+	public Sprite[] sprites;
 
 	void Awake () {
 		
 		// インスペクタで初期化してなかった時の処理
-		if (currentBulletUI == null) 	currentBulletUI	= GameObject.Find ("CurrentBulletUI");
-		if (nextBulletUI == null) 		nextBulletUI = GameObject.Find ("NextBulletUI");
-		if (nextnextBulletUI == null)	nextnextBulletUI = GameObject.Find ("NextNextBulletUI");
+		if (currentBulletImage == null)  currentBulletImage	= GameObject.Find ("CurrentBulletImage");
+		if (nextBulletImage == null) 	 nextBulletImage = GameObject.Find ("NextBulletImage");
+		if (nextnextBulletImage == null) nextnextBulletImage = GameObject.Find ("NextNextBulletImage");
 
+		if (magazine == null) magazine = GameObject.Find ("Magazine");
 	}
 
 	// Use this for initialization
 	void Start () {
 
-		// Magazineを取得する
-		if (magazine == false) {
-			magazine = GameObject.Find ("Magazine");
-		}
 
+
+		bullet [0] = BulletType.Spoon;
+		bullet [1] = BulletType.Spoon;
+		bullet [2] = BulletType.Spoon;
+
+		Init ();
+
+		ShowUI (); // test
+	}
+
+	void Init () {
+		
 		// Playerが選択中のShooter番号を取得する
+		selectedShooterNum = 0; // test
 
 		// Shooter番号のMagazineを取得する
-
-		// Magazine[Shooter番号]の、３つのBulletをcurrentBullet, nextBullet, nextnextBullet
-		currentBullet = GameObject.Find ("Bullet");
-
-		// test
-		ShowUI ();
+		bullet [0] = magazine.GetComponent<Magaizine>().NowBullet(selectedShooterNum);
+		bullet [1] = magazine.GetComponent<Magaizine> ().NextBullet (selectedShooterNum);
+		bullet [2] = magazine.GetComponent<Magaizine> ().NextNextBullet (selectedShooterNum);
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
 		// Playerが操作している（選択している）Shooterが切り替わったら、装填されてる弾を変更する
-		if (true /*Inputで切り替わる*/) {
+		if (true /*selecetedShooterが切り替わる*/) {
 
 			ChangeMagazine ();
 		}
@@ -68,12 +83,22 @@ public class NextBoxUI : MonoBehaviour {
 	}
 
 	void ShowUI () {
-		GameObject.Instantiate (currentBullet, currentBulletUI.transform.position, currentBulletUI.transform.rotation);
+		// test
+		var currentBullet = GameObject.Instantiate (bulletPrefab, currentBulletImage.transform.position, currentBulletImage.transform.rotation) as GameObject;
 		//GameObject currentBullet = GameObject.Instantiate (currentBullet, currentBullet.transform.position, currentBullet.transform.rotation);
 		currentBullet.transform.parent = transform;
 
-		currentBullet.transform.position = currentBulletUI.GetComponent<RectTransform> ().transform.position;
-		print ("currentBulletUI Pos: " + currentBulletUI.GetComponent<RectTransform>().transform.position);
+		currentBullet.transform.position = currentBulletImage.GetComponent<RectTransform> ().transform.position;
+		print ("currentBulletUI Pos: " + currentBulletImage.GetComponent<RectTransform>().transform.position);
+
+
+		// 本番
+		// Shooter番号のMagazineを取得する
+		bullet [0] = magazine.GetComponent<Magaizine>().NowBullet(selectedShooterNum);
+		bullet [1] = magazine.GetComponent<Magaizine> ().NextBullet (selectedShooterNum);
+		bullet [2] = magazine.GetComponent<Magaizine> ().NextNextBullet (selectedShooterNum);
+
+		// bulletを表示する
 
 
 
